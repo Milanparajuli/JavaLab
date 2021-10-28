9adde6eaf1dcc7286507669a2badf330393a6489
Milanparajuli <milanbhai2058@gmail.com>
2021-10-28 18:23:13 +0545

    add mySql

---
+++ JDBC2.java
@@ -0,0 +1,26 @@
+package milan.lab.report;
+
+import java.sql.*;
+
+class JDBC2{
+public static void main(String[] args){
+try{
+Class.forName("com.mysql.cj.jdbc.Driver");
+String url = "jdbc:mysql://localhost:3306/bca_third_2020?serverTimezone=UTC";
+String username = "root";
+String password = "";
+Connection cn = DriverManager.getConnection(url,username,password);
+insert(cn);
+}catch(Exception e){
+System.out.println(e);
+}
+}
+
+private static void insert(Connection cn) throws SQLException {
+Statement stat = cn.createStatement(); // statement create
+
+String query = "insert into student (roll_no, name, course) values (5, 'Milan', 'BCA')";
+stat.execute(query); // execute query
+System.out.println("Record inserted!");
+}
+}
